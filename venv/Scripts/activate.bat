3a0c36219b495a01503c7b481a0da844ec8fee11
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-04-30 03:06:18 -0600

    venv

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\anibal\Desktop\django-crud-react\venv
+set VIRTUAL_ENV=C:\Users\anibal-alpizar\Desktop\django-crud-react\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
